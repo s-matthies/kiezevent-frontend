2d7da3405e87c969563b6baab214f9528f81a39d
Sabine Matthies <Sabine.Matthies@student.htw-berlin.de>
2024-03-13 21:59:52 +0100

    BackendService hinzugefuegt und Methoden zum Erstellen und Abbrechen eines Events

--- src/app/create-event/create-event.component.ts
+++ src/app/create-event/create-event.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
-import { FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Component, OnInit, inject } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { BackendService } from '../shared/backend.service';
 
 @Component({
   selector: 'app-create-event',
@@ -8,14 +9,54 @@ import { FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
   templateUrl: './create-event.component.html',
   styleUrl: './create-event.component.css'
 })
-export class CreateEventComponent {
+export class CreateEventComponent implements OnInit {
 
-  titleFC = new FormControl('', [Validators.required, Validators.minLength(3)]); // FormControl für den Titel des Events
+
+  // FormControl-Objekte für die Eingabefelder des Formulars
+  titleFC = new FormControl('', [Validators.required]);
+  descriptionFC = new FormControl('', [Validators.required]);
   dateFC = new FormControl('', [Validators.required]);
-  starttimeFC = new FormControl('', [Validators.required]); 
-  endtimeFC = new FormControl('', [Validators.required]); 
-  locationFC = new FormControl('', [Validators.required]); 
-  descriptionFC = new FormControl('', [Validators.required, Validators.minLength(10)]); 
+  starttimeFC = new FormControl('', [Validators.required]);
+  endtimeFC = new FormControl('', [Validators.required]);
+  locationFC = new FormControl('', [Validators.required]);
+  priceFC = new FormControl('', [Validators.required]);
+  maxParticipantsFC = new FormControl('', [Validators.required]);
 
+  bs = inject (BackendService); // BackendService per Dependency Injection einbinden
+  backendService: any;
+  
+  // ngOnInit-Methode zum Initialisieren des BackendService
+  ngOnInit(): void {
+    this.create();
+    };
 
-}
+ 
+  // Methode zum Erstellen eines Events
+  create() { 
+    this.backendService.createEvent({
+      title: this.titleFC.value,
+      description: this.descriptionFC.value,
+      date: this.dateFC.value,
+      starttime: this.starttimeFC.value,
+      endtime: this.endtimeFC.value,
+      location: this.locationFC.value,
+      price: this.priceFC.value,
+      maxParticipants: this.maxParticipantsFC.value
+    }).subscribe((response: any) => { // Response-Objekt ausgeben lassen (zum Testen)
+      console.log(response);
+    });
+    }
+
+    // Methode zum Abbrechen des Erstellens eines Events
+    cancel() {
+      this.titleFC.setValue('');
+      this.descriptionFC.setValue('');
+      this.dateFC.setValue('');
+      this.starttimeFC.setValue('');
+      this.endtimeFC.setValue('');
+      this.locationFC.setValue('');
+      }
+  }
+
+  
+
