145110800510560f618cfd3f68f0052d12e17b89
Sabine Matthies <Sabine.Matthies@student.htw-berlin.de>
2024-03-13 12:28:07 +0100

    Eventdetail-Komponente hinzugefuegt

---
+++ src/app/eventdetail/eventdetail.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-eventdetail',
+  standalone: true,
+  imports: [],
+  templateUrl: './eventdetail.component.html',
+  styleUrl: './eventdetail.component.css'
+})
+export class EventdetailComponent {
+
+}
