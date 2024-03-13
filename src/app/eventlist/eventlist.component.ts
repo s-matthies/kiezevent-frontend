9f58863762cce485b8a0ce72000bc282235a6e20
Sabine Matthies <Sabine.Matthies@student.htw-berlin.de>
2024-03-13 18:49:17 +0100

    OnInit-Implementierung und Backend-Service hinzugefuegt

--- src/app/eventlist/eventlist.component.ts
+++ src/app/eventlist/eventlist.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { BackendService } from '../shared/backend.service';
+import { Event } from '../shared/event';
 
 @Component({
   selector: 'app-eventlist',
@@ -7,6 +9,26 @@ import { Component } from '@angular/core';
   templateUrl: './eventlist.component.html',
   styleUrl: './eventlist.component.css'
 })
-export class EventlistComponent {
+export class EventlistComponent implements OnInit{
 
+  events!: Event[]; // ! bedeutet das, dass die Variable initialisiert wird, bevor sie benutzt wird
+
+  constructor(private bs : BackendService) { }
+
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+
+  readAll(): void {
+    this.bs.getAllEvents().subscribe( // BackendService aufrufen
+          {
+            next: (response) => {
+                  this.events = response;
+                  console.log(this.events);
+                  return this.events;
+                },
+            error: (err) => console.log(err),
+            complete: () => console.log('getAllEvents() completed')
+          })
+  }
 }
