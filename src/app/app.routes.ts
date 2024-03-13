3653890a61ed75515eb62e646ec274cb62cfd048
Sabine Matthies <Sabine.Matthies@student.htw-berlin.de>
2024-03-13 14:13:33 +0100

    Routen und Importe für Eventliste und edit-event aktualisiert

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,9 +6,8 @@ import { CreateEventComponent } from './create-event/create-event.component';
 
 export const routes: Routes = [
     { path: "detail", component: EventdetailComponent },
-    { path: "list", component: EventlistComponent },
+    { path: "events", component: EventlistComponent },
     { path: "create", component: CreateEventComponent },
-    { path: "eventlist", component: EventlistComponent },
     { path: "", component: HomeComponent, pathMatch: 'full'} // default route bedeutet das, wenn keine Route angegeben ist, dann wird HomeComponent geladen
     // pathMatch: 'full' bedeutet, dass die Route nur dann geladen wird, wenn der Pfad exakt übereinstimmt
 ];
--- src/app/edit-event/edit-event.component.ts
+++ src/app/edit-event/edit-event.component.ts
@@ -1,9 +1,11 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-edit-event',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './edit-event.component.html',
   styleUrl: './edit-event.component.css'
 })
