e7d64772d4d2ddc4e517786c14aaa4ef30c9a0ee
Sabine Matthies <Sabine.Matthies@student.htw-berlin.de>
2024-03-13 19:57:24 +0100

    FormControl für Event-Daten hinzugefuegt

--- src/app/create-event/create-event.component.ts
+++ src/app/create-event/create-event.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
+import { FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-create-event',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './create-event.component.html',
   styleUrl: './create-event.component.css'
 })
 export class CreateEventComponent {
+
+  titleFC = new FormControl('', [Validators.required, Validators.minLength(3)]); // FormControl für den Titel des Events
+  dateFC = new FormControl('', [Validators.required]);
+  starttimeFC = new FormControl('', [Validators.required]); 
+  endtimeFC = new FormControl('', [Validators.required]); 
+  locationFC = new FormControl('', [Validators.required]); 
+  descriptionFC = new FormControl('', [Validators.required, Validators.minLength(10)]); 
+
 
 }
