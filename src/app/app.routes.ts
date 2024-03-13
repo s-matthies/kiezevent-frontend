f8e5e848993658a44c6d64eb05613556771a2a8b
Sabine Matthies <Sabine.Matthies@student.htw-berlin.de>
2024-03-13 18:48:29 +0100

    Routen in app.routes.ts aktualisiert

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,8 +5,8 @@ import { EventlistComponent } from './eventlist/eventlist.component';
 import { CreateEventComponent } from './create-event/create-event.component';
 
 export const routes: Routes = [
-    { path: "detail", component: EventdetailComponent },
-    { path: "events", component: EventlistComponent },
+    { path: "event/:id", component: EventdetailComponent },
+    { path: "event", component: EventlistComponent },
     { path: "create", component: CreateEventComponent },
     { path: "", component: HomeComponent, pathMatch: 'full'} // default route bedeutet das, wenn keine Route angegeben ist, dann wird HomeComponent geladen
     // pathMatch: 'full' bedeutet, dass die Route nur dann geladen wird, wenn der Pfad exakt übereinstimmt
