307117f8cf7cfa1ed27e589e477883d4c9afaf77
Sabine Matthies <Sabine.Matthies@student.htw-berlin.de>
2024-03-14 19:04:22 +0100

    Paginierung hinzugefuegt & Style aktualisiert

--- src/app/eventlist/eventlist.component.ts
+++ src/app/eventlist/eventlist.component.ts
@@ -1,35 +1,72 @@
 import { Component, OnInit, inject } from '@angular/core';
 import { BackendService } from '../shared/backend.service';
 import { Event } from '../shared/event';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-eventlist',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './eventlist.component.html',
   styleUrl: './eventlist.component.css'
 })
 export class EventlistComponent implements OnInit{ // Oninit -> Lifecycle-Hook, der beim Initialisieren der Komponente aufgerufen wird
 
   events: Event[] = []; // Array für die Events
-
   bs = inject (BackendService); // BackendService per Dependency Injection einbinden
   // andere Möglichkeit: constructor(private bs : BackendService) { }
 
+  pages: number[] = []; // Array für die Seitenzahlen
+  totalPages: number = 0;; // Gesamtanzahl der Seiten
+
+  pagedEvents: Event[] = []; // Array für die Events auf der aktuellen Seite
+  currentPage = 1; // Aktuelle Seite
+  pageSize = 9; // Anzahl der Events pro Seite
+ 
   ngOnInit(): void {
     this.readAllEvents(); // beim Initialisieren der Komponente alle Events auslesen
   }
 
   readAllEvents(): void {
-    this.bs.getAllEvents().subscribe( // BackendService aufrufen // subscribe() -> wir melden uns an
-          {
-            next: (response) => { // next() -> gibt uns den eigentlichen Wert zurück; wir reagieren auf die Antwort
-                  this.events = response;
-                  console.log("events", this.events);
-                  return this.events;
-                },
-            error: (err) => console.log(err),
-            complete: () => console.log('getAllEvents() completed')
-          })
+    this.bs.getAllEvents().subscribe(
+      (response) => {
+        this.events = response;
+        this.events.sort((a, b) => new Date(a.date).getTime() - new Date(b.date).getTime()); // Events nach Datum sortieren
+        this.totalPages = Math.ceil(this.events.length / this.pageSize); // Berechnen Sie die Gesamtzahl der Seiten
+        this.pages = Array.from({length: this.totalPages}, (_, i) => i + 1); // Erstellen Sie ein Array mit Seitennummern
+        this.updatePage(); // Seite aktualisieren, um Events zu paginieren
+      },
+      (error) => console.log(error)
+    );
+  }
+
+  // Pagination
+  updatePage(): void {
+    const startIndex = (this.currentPage - 1) * this.pageSize;
+    this.pagedEvents = this.events.slice(startIndex, startIndex + this.pageSize);
+  }
+
+  setPage(page: number): void {
+    this.currentPage = page;
+    this.updatePage();
+  }
+
+  previousPage(): void {
+    if (this.currentPage > 1) {
+      this.currentPage--;
+      this.updatePage();
+    }
+  }
+
+  nextPage(): void {
+    const totalPages = Math.ceil(this.events.length / this.pageSize);
+    if (this.currentPage < totalPages) {
+      this.currentPage++;
+      this.updatePage();
+    }
   }
 }
+
+        
+          
