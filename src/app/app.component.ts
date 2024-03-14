d84c06e7f43ed086b4c4ee4e045d1d46140a5a03
Sabine Matthies <Sabine.Matthies@student.htw-berlin.de>
2024-03-14 10:16:48 +0100

    NavComponent-Import aus AppComponent entfernt, nicht genutzt

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { FooterComponent } from "./footer/footer.component";
-import { NavComponent } from "./nav/nav.component";
 import { HeaderComponent } from "./header/header.component";
 import { HomeComponent } from "./home/home.component";
 
@@ -11,7 +10,7 @@ import { HomeComponent } from "./home/home.component";
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [CommonModule, RouterOutlet, FooterComponent, NavComponent, HeaderComponent, HomeComponent]
+    imports: [CommonModule, RouterOutlet, FooterComponent, HeaderComponent, HomeComponent]
 })
 export class AppComponent {
   title = 'kiezevent-frontend';
