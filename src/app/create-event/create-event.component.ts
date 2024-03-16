a77cad014298071572248599802524d3800daf60
Sabine Matthies <Sabine.Matthies@student.htw-berlin.de>
2024-03-16 15:40:27 +0100

    Update create-event.component.html und create-event.component.ts - creat() aktualisiert

--- src/app/create-event/create-event.component.ts
+++ src/app/create-event/create-event.component.ts
@@ -1,60 +1,74 @@
 import { Component, OnInit, inject } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
 import { BackendService } from '../shared/backend.service';
-import { CommonModule } from '@angular/common';
+import { CommonModule, Time } from '@angular/common';
+import { NgbDatepickerModule } from '@ng-bootstrap/ng-bootstrap';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-create-event',
   standalone: true,
-  imports: [CommonModule, ReactiveFormsModule],
+  imports: [CommonModule, ReactiveFormsModule, NgbDatepickerModule],
   templateUrl: './create-event.component.html',
   styleUrl: './create-event.component.css'
 })
-export class CreateEventComponent implements OnInit {
+export class CreateEventComponent {
 
 
   // FormControl-Objekte für die Eingabefelder des Formulars
   titleFC = new FormControl('', [Validators.required]);
-  descriptionFC = new FormControl('', [Validators.required]);
-  dateFC = new FormControl('', [Validators.required]);
-  starttimeFC = new FormControl('', [Validators.required]);
-  endtimeFC = new FormControl('', [Validators.required]);
+  dateFC = new FormControl(new Date(), [Validators.required]);
+  starttimeFC = new FormControl({ hours: 0, minutes: 0 } as Time, [Validators.required]); // Anfangswerte für starttime und endtime festlegen
+  endtimeFC = new FormControl({ hours: 0, minutes: 0 } as Time, [Validators.required]);
   locationFC = new FormControl('', [Validators.required]);
+  descriptionFC = new FormControl('', [Validators.required]);
   linkFC = new FormControl('',[Validators.pattern('^(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,})([\/\w \.-]*)*\/?$')]);
 
   bs = inject (BackendService); // BackendService per Dependency Injection einbinden
-  backendService: any;
-  
-  // ngOnInit-Methode zum Initialisieren des BackendService
-  ngOnInit(): void {
-    this.create();
-    };
+  private router = inject(Router); //
+
+
+
+  private formValid() {
+    return this.titleFC.valid && this.descriptionFC.valid && this.dateFC.valid && this.starttimeFC.valid && this.endtimeFC.valid && this.locationFC.valid && this.linkFC.valid;
+  }
 
  
   // Methode zum Erstellen eines Events
   create() { 
-    this.backendService.createEvent({
-      title: this.titleFC.value,
-      description: this.descriptionFC.value,
-      date: this.dateFC.value,
-      starttime: this.starttimeFC.value,
-      endtime: this.endtimeFC.value,
-      location: this.locationFC.value,
-      link: this.linkFC.value,
-    }).subscribe((response: any) => { // Response-Objekt ausgeben lassen (zum Testen)
-      console.log(response);
-    });
+    console.log('create() wurde aufgerufen'); // debug
+    if (this.formValid()) {
+      let event = {
+        title: this.titleFC.value!,
+        date: this.dateFC.value!,
+        starttime: this.starttimeFC.value!,
+        endtime: this.endtimeFC.value!,
+        location: this.locationFC.value!,
+        description: this.descriptionFC.value!,
+        link: this.linkFC.value
+      };
+      console.log('event wurde aufgerufen: ', event); // debug
+
+      this.bs.createEvent(event).subscribe({
+        next: (response) => {
+          console.log('Event erfolgreich erstellt: ', response);
+        },
+        error: (err) => console.log('Fehler beim Erstellen des Events: ', err),
+        complete: () => console.log('create() completed')
+      });
+      
+    }
     }
 
     // Methode zum Abbrechen des Erstellens eines Events
     cancel() {
-      this.titleFC.setValue('');
-      this.descriptionFC.setValue('');
-      this.dateFC.setValue('');
-      this.starttimeFC.setValue('');
-      this.endtimeFC.setValue('');
-      this.locationFC.setValue('');
-      this.linkFC.setValue('');
+      this.titleFC.reset();
+      this.descriptionFC.reset();
+      this.dateFC.reset();
+      this.starttimeFC.reset(); 
+      this.endtimeFC.reset();
+      this.locationFC.reset();
+      this.linkFC.reset();
       }
   }
 
