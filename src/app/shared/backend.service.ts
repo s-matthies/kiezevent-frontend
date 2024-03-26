b12a30bb6c0e349ad55c17cb5b784400335a8506
Sabine Matthies <Sabine.Matthies@student.htw-berlin.de>
2024-03-26 20:03:50 +0100

    Update Backend-URL in BackendService

--- src/app/shared/backend.service.ts
+++ src/app/shared/backend.service.ts
@@ -2,36 +2,38 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Event } from './event';
 import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class BackendService {
 
-  backendUrl = "https://kiezevent-backend.onrender.com"; // Basis-URL für die REST-Schnittstelle
+  baseUrl = environment.apiUrl;
+
+  // backendUrl = "https://kiezevent-backend.onrender.com"; // Basis-URL für die REST-Schnittstelle
 
   constructor(private http: HttpClient) { } // einbinden HttpClient per Dependency Injection
 
   getAllEvents(): Observable<Event[]> {
-    return this.http.get<Event[]>(this.backendUrl + "/events"); // GET-Request an die REST-Schnittstelle senden 
+    return this.http.get<Event[]>(this.baseUrl + "/events"); // GET-Request an die REST-Schnittstelle senden 
   }
 
   getEventById(id: number): Observable<Event> {
-    return this.http.get<Event>(this.backendUrl + "/events/" + id); // GET-Request an die REST-Schnittstelle senden
+    return this.http.get<Event>(this.baseUrl + "/events/" + id); // GET-Request an die REST-Schnittstelle senden
   }
 
   createEvent(event: Event): Observable<Event> {
-    return this.http.post<Event>(this.backendUrl + "/events", event); // POST-Request an die REST-Schnittstelle senden
+    return this.http.post<Event>(this.baseUrl + "/events", event); // POST-Request an die REST-Schnittstelle senden
   }
   
 
   updateOneMember(event: Event, id: number): Observable<Event> {
-    return this.http.put<Event>(this.backendUrl + "/events/" + id, event);
+    return this.http.put<Event>(this.baseUrl + "/events/" + id, event);
   }
 
   deleteEvent(id: number): Observable<Event> {
-    return this.http.delete<Event>(this.backendUrl + "/events/" + id); // DELETE-Request an die REST-Schnittstelle senden
+    return this.http.delete<Event>(this.baseUrl + "/events/" + id); 
   }
-
-  // weitere Methoden für die REST-Schnittstelle
 }
