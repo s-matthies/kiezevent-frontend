386c1c6b8325661d7b0c5768915cebc61e38a0a0
Sabine Matthies <Sabine.Matthies@student.htw-berlin.de>
2024-03-26 20:01:11 +0100

    Update CreateEventComponent CSS und TS-Dateien

--- src/app/create-event/create-event.component.ts
+++ src/app/create-event/create-event.component.ts
@@ -14,15 +14,15 @@ import { Observable, of } from 'rxjs';
   templateUrl: './create-event.component.html',
   styleUrl: './create-event.component.css'
 })
-export class CreateEventComponent{
-  
+export class CreateEventComponent {
+
   private modalService = inject(NgbModal);
   private bs = inject(BackendService);
   private router = inject(Router);
   closeResult = '';
-  event!: Event; 
+  event!: Event;
   submitClicked = false; // Diese Variable wird auf true gesetzt, wenn der "Hinzufügen"-Button geklickt wird
-  
+
   titleFC = new FormControl('', [Validators.required]);
   dateFC = new FormControl(null, [Validators.required], futureDateValidator());
   starttimeFC = new FormControl(null, [Validators.required]);
@@ -31,7 +31,7 @@ export class CreateEventComponent{
   descriptionFC = new FormControl('', [Validators.required]);
   linkFC = new FormControl('', urlValidator());
 
-  
+
 
   //
   private formValid() {
@@ -43,8 +43,7 @@ export class CreateEventComponent{
   createEvent(content: TemplateRef<any>) {
     this.submitClicked = true;
 
-    if(this.formValid())
-    {
+    if (this.formValid()) {
       let event = {
         title: this.titleFC.value!,
         date: this.dateFC.value!,
@@ -54,94 +53,88 @@ export class CreateEventComponent{
         description: this.descriptionFC.value!,
         link: this.linkFC.value!
       }
-      
+
       console.log('new event: ', event);
 
       this.bs.createEvent(event).subscribe({
         next: (response) => console.log('response', response),
         error: (err) => console.log(err),
         complete: () => console.log('register completed')
-    });
+      });
 
 
-   
-
-
-    // Modal öffnen
-    this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title' }).result
-      .then(
-        (result) => {
-          this.closeResult = `Closed with: ${result}`;
-          this.router.navigate(['/event']);
-        },
-        (reason) => {
-          this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
-        },
-      );
+      // Modal öffnen
+      this.modalService.open(content, { ariaLabelledBy: 'modal-basic-title' }).result
+        .then(
+          (result) => {
+            this.closeResult = `Closed with: ${result}`;
+            this.router.navigate(['/event']);
+          },
+          (reason) => {
+            this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+          },
+        );
 
       console.log('new event: ', event)
     }
-    else
-    {
+    else {
       console.warn('form still invalid!')
     }
   }
 
-      // Methode zum Anzeigen des Modal-Dialogs
-      private getDismissReason(reason: any): string {
-        switch (reason) {
-          case ModalDismissReasons.ESC:
-            return 'by pressing ESC';
-          case ModalDismissReasons.BACKDROP_CLICK:
-            return 'by clicking on a backdrop';
-          default:
-            return `with: ${reason}`;
-        }
-      }
-
-
-      // Hilfsmethode für die Anzeige von Fehlermeldungen
-      isInvalidAndTouched(field: FormControl): boolean {
-        return !field.valid && field.touched;
-      }
-
-
-      // Methode zum Zurücksetzen des Formulars
-      resetForm(): void {
-        this.titleFC.reset();
-        this.dateFC.reset();
-        this.starttimeFC.reset();
-        this.endtimeFC.reset();
-        this.locationFC.reset();
-        this.descriptionFC.reset();
-        this.linkFC.reset();
-      }
-
-
-      // Methode zum Abbrechen / zurück zur EventList-Komponente
-    cancel() {
-      this.router.navigate(['/event']); // zurück zur EventList-Komponente navigieren
-     }
-
-     
+  // Methode zum Anzeigen des Modal-Dialogs
+  private getDismissReason(reason: any): string {
+    switch (reason) {
+      case ModalDismissReasons.ESC:
+        return 'by pressing ESC';
+      case ModalDismissReasons.BACKDROP_CLICK:
+        return 'by clicking on a backdrop';
+      default:
+        return `with: ${reason}`;
+    }
   }
 
-  // Validator für das Datum (darf nicht in der Vergangenheit liegen)
-  export function futureDateValidator(): AsyncValidatorFn {
-    return (control: AbstractControl): Observable<ValidationErrors | null> => {
-      const forbidden = new Date(control.value) < new Date();
-      return of(forbidden ? { 'pastDate': {value: control.value} } : null);
-    };
+
+  // Hilfsmethode für die Anzeige von Fehlermeldungen
+  isInvalidAndTouched(field: FormControl): boolean {
+    return !field.valid && field.touched;
   }
-  // Validator für den Link (muss ein gültiger URL sein, wenn vorhanden)
-  export function urlValidator(): ValidatorFn {
-    return (control: AbstractControl): ValidationErrors | null => {
-      const value = control.value;
-      if (!value) { // wenn kein Wert vorhanden ist, ist die Validierung erfolgreich
-        return null;
-      }
-      // Überprüfen Sie, ob der Wert ein gültiger URL ist
-      const isValidUrl = /^(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?$/.test(value);
-      return isValidUrl ? null : { 'invalidUrl': {value: control.value} };
-    };
+
+
+  // Methode zum Zurücksetzen des Formulars
+  resetForm(): void {
+    this.titleFC.reset();
+    this.dateFC.reset();
+    this.starttimeFC.reset();
+    this.endtimeFC.reset();
+    this.locationFC.reset();
+    this.descriptionFC.reset();
+    this.linkFC.reset();
   }
+
+
+  // Methode zum Abbrechen / zurück zur EventList-Komponente
+  cancel() {
+    this.router.navigate(['/event']); // zurück zur EventList-Komponente navigieren
+  }
+}
+
+// Validator für das Datum (darf nicht in der Vergangenheit liegen)
+export function futureDateValidator(): AsyncValidatorFn {
+  return (control: AbstractControl): Observable<ValidationErrors | null> => {
+    const forbidden = new Date(control.value) < new Date();
+    return of(forbidden ? { 'pastDate': { value: control.value } } : null);
+  };
+}
+// Validator für den Link (muss ein gültiger URL sein, wenn vorhanden)
+export function urlValidator(): ValidatorFn {
+  return (control: AbstractControl): ValidationErrors | null => {
+    const value = control.value;
+    if (!value) { // wenn kein Wert vorhanden ist, ist die Validierung erfolgreich
+      return null;
+    }
+    // Überprüfen Sie, ob der Wert ein gültiger URL ist
+    const isValidUrl = /^(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?$/.test(value);
+    return isValidUrl ? null : { 'invalidUrl': { value: control.value } };
+  };
+}
